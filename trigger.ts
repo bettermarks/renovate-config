05635e48c4d54a7343863573c3d36a87c6cb5fd2
karfau <christian.bewernitz@bettermarks.com>
2025-10-18 17:23:23 +0200

    fix: improve logs and handle edge case

--- trigger.ts
+++ trigger.ts
@@ -3,17 +3,26 @@ import defaultJson from "./default.json" with { type: "json" };
 import { writeFileSync } from "node:fs";
 import path from "node:path";
 
+/**
+ * Receives process.env.OWNER_REPO and optional process.env.DASHBOARD_ISSUE,
+ * to check the box for a manual trigger on the dependency dashboard.
+ * If DASHBOARD_ISSUE is not provided, it tries to detect it.
+ *
+ * If another checkbox is checked in the issue, nothing will be triggered.
+ * The function executed the `gh` CLI to talk to the GitHub API.
+ */
 export const trigger = async ({ exec }: AsyncFunctionArguments) => {
-  const OWNER_REPO = process.env.OWNER_REPO ?? 'bettermarks/renovate-config';
+  // output auth info for debugging
+  await exec.getExecOutput("gh", ["auth", "status"]);
+  const OWNER_REPO = process.env.OWNER_REPO ?? "bettermarks/renovate-config";
   let dashboardIssue = undefined;
-  console.log((await exec.getExecOutput("gh", ["auth", "status"])).stdout);
-  if (process.env.dashboardIssue) {
+  if (process.env.DASHBOARD_ISSUE) {
     dashboardIssue = JSON.parse(
       (
         await exec.getExecOutput("gh", [
           "issue",
           "view",
-          process.env.dashboardIssue,
+          process.env.DASHBOARD_ISSUE,
           "--repo",
           OWNER_REPO,
           "--json",
@@ -38,7 +47,8 @@ export const trigger = async ({ exec }: AsyncFunctionArguments) => {
         ])
       ).stdout,
     );
-    if (!Array.isArray(parsed)) throw parsed;
+    if (!Array.isArray(parsed))
+      throw "JSON.parse should have returned an Array!";
     dashboardIssue = parsed.find(
       (it) => it.title === defaultJson.dependencyDashboardTitle,
     );
@@ -46,20 +56,24 @@ export const trigger = async ({ exec }: AsyncFunctionArguments) => {
   if (!dashboardIssue) throw `Not able to get dashboard issue`;
   console.log("detected ", dashboardIssue.url);
   if (dashboardIssue.body.includes("- [x]")) {
-    return "Dashboard already contains checked checkbox, returning.";
+    console.log(
+      "Dashboard already contains checked checkbox, not triggering manual update.",
+    );
+    return;
+  }
+  const manualTrigger = "- [ ] <!-- manual job -->";
+  if (!dashboardIssue.body.includes(manualTrigger)) {
+    throw "Dashboard does not contain manual trigger checkbox, exiting.";
   }
   console.log(
-    `Triggering renovate on ${OWNER_REPO}#${dashboardIssue.number}...`,
+    `Triggering renovate via ${OWNER_REPO}#${dashboardIssue.number}...`,
   );
   const bodyFile = path.join(import.meta.dirname, ".dashboard-body.md");
   writeFileSync(
     bodyFile,
-    dashboardIssue.body.replace(
-      "- [ ] <!-- manual job -->",
-      "- [x] <!-- manual job -->",
-    ),
+    dashboardIssue.body.replace(manualTrigger, "- [x] <!-- manual job -->"),
   );
-  exec.getExecOutput("gh", [
+  await exec.getExecOutput("gh", [
     "issue",
     "edit",
     dashboardIssue.number,
@@ -68,5 +82,5 @@ export const trigger = async ({ exec }: AsyncFunctionArguments) => {
     "--body-file",
     bodyFile,
   ]);
-  return "Done."
+  return "Done.";
 };
