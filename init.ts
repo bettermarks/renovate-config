cf8610b3596be3f60dba52668d1e235ccc4f57af
karfau <christian.bewernitz@bettermarks.com>
2025-10-18 15:18:03 +0200

    feat: add script and workflow to trigger renovate via dashboard
    As part of that add minimal modern typescript tooling, as we are used to it in other repos.

---
+++ init.ts
@@ -0,0 +1,55 @@
+import fs from "node:fs";
+import path from "node:path";
+
+console.log("process.argv:", JSON.stringify(process.argv));
+const [_nodePath, _scriptPath, target, inputsRaw = "{}"] = process.argv;
+
+const inputs = JSON.parse(inputsRaw);
+const config = {
+  $schema: "https://docs.renovatebot.com/renovate-schema.json",
+  extends: [
+    `github>bettermarks/renovate-config${inputs.javascript ? ":javascript" : ""}`,
+    inputs.python ? "github>bettermarks/renovate-config:python" : "",
+    inputs.automerge,
+  ].filter(Boolean),
+};
+console.log(config);
+if (!target || !fs.existsSync(target)) {
+  throw (
+    "first argument target has to be a path to a directory but does not exist, was " +
+    target
+  );
+}
+let configFile = path.join(target, "renovate.json");
+if (!fs.existsSync(configFile) || inputs.force) {
+  fs.writeFileSync(configFile, JSON.stringify(config, null, 2), "utf-8");
+  console.error(`written to ${configFile}`);
+} else {
+  console.error(`NOT modifying existing ${configFile}!`);
+}
+
+let relativeWorkflowFile = ".github/workflows/renovate-config-validator.yml";
+let workflowFile = path.join(target, relativeWorkflowFile);
+if (!fs.existsSync(workflowFile) || inputs.force) {
+  fs.cpSync(relativeWorkflowFile, workflowFile);
+  console.error(`created ${workflowFile}!`);
+} else {
+  console.error(`NOT modifying existing ${workflowFile}!`);
+}
+
+if (inputs.javascript) {
+  const npmrcFile = path.join(target, ".npmrc");
+  const saveExact = `save-exact=true`;
+  const existingContent =
+    fs.existsSync(npmrcFile) && fs.readFileSync(npmrcFile, "utf8");
+  let write: string | false = false;
+  if (!existingContent) {
+    write = `${saveExact}\n`;
+  } else if (!/^save-exact/.test(existingContent)) {
+    write = `${existingContent}\n${saveExact}\n`;
+  }
+  if (write) {
+    fs.writeFileSync(npmrcFile, write);
+    console.error(`added "${saveExact}" to ${npmrcFile}!`);
+  }
+}
---
+++ trigger.ts
@@ -0,0 +1,32 @@
+import type { AsyncFunctionArguments } from "@actions/github-script";
+import defaultJson from "./default.json" with { type: 'json' };
+
+export const trigger = async ({ exec }: AsyncFunctionArguments) => {
+  // inputs
+  let dashboardIssue = undefined;
+  if (process.env.dashboardIssue) {
+    dashboardIssue = JSON.parse(
+      (
+        await exec.getExecOutput(
+          "gh",
+          ["issue", "view", `"${process.env.dashboardIssue}"`, "--json"],
+          {},
+        )
+      ).stdout,
+    );
+  } else {
+    const parsed = JSON.parse(
+      (
+        await exec.getExecOutput(
+          "gh",
+          ["api", "--author", "renovate", "--state", "open", "--json"],
+          {},
+        )
+      ).stdout,
+    );
+    if (!Array.isArray(parsed)) throw parsed;
+    dashboardIssue = parsed.find((it) => it.title === defaultJson.dependencyDashboardTitle);
+  }
+  if (!dashboardIssue) throw `Not able to get dashboard issue`;
+  console.log(JSON.stringify(dashboardIssue, null, 2));
+};
