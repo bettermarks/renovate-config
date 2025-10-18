4a9c3be7d60dc58998b623dfb1634d7921284f50
karfau <christian.bewernitz@bettermarks.com>
2025-10-18 20:46:33 +0200

    refactor: extract functions and types
    for less boilerplate

--- trigger.ts
+++ trigger.ts
@@ -12,62 +12,54 @@ import path from "node:path";
  * The function executed the `gh` CLI to talk to the GitHub API.
  */
 export const trigger = async ({ exec }: AsyncFunctionArguments) => {
-  // output auth info for debugging
-  await exec.getExecOutput("gh", ["auth", "status"]);
+  const { gh, ghJson } = ghExec(exec);
+  await gh("auth", "status");
+
+  // parse input
   const OWNER_REPO = process.env.OWNER_REPO ?? "bettermarks/renovate-config";
-  const DASHBOARD_ISSUE = parseInt(process.env.DASHBOARD_ISSUE || '0');
+  const DASHBOARD_ISSUE = parseInt(process.env.DASHBOARD_ISSUE || "0");
   console.log("Inputs:", { OWNER_REPO, DASHBOARD_ISSUE });
 
-  let dashboardIssue = undefined;
+  let dashboardIssue: GithubIssue | undefined;
   if (DASHBOARD_ISSUE) {
-    dashboardIssue = JSON.parse(
-      (
-        await exec.getExecOutput("gh", [
-          "issue",
-          "view",
-          `${DASHBOARD_ISSUE}`,
-          "--repo",
-          OWNER_REPO,
-          "--json",
-          "url,number,body,title",
-        ])
-      ).stdout,
+    dashboardIssue = await ghJson<GithubIssue>(
+      "issue",
+      "view",
+      `${DASHBOARD_ISSUE}`,
+      "--repo",
+      OWNER_REPO,
+      "--json",
+      "url,number,body,title",
     );
   } else {
-    const parsed = JSON.parse(
-      (
-        await exec.getExecOutput("gh", [
-          "issue",
-          "list",
-          "--repo",
-          OWNER_REPO,
-          "--author",
-          "renovate[bot]",
-          "--state",
-          "open",
-          "--json",
-          "url,number,body,title",
-        ])
-      ).stdout,
+    const parsed = await ghJson<ReadonlyArray<GithubIssue> | unknown>(
+      "issue",
+      "list",
+      "--repo",
+      OWNER_REPO,
+      "--author",
+      "renovate[bot]",
+      "--state",
+      "open",
+      "--json",
+      "url,number,body,title",
     );
     if (!Array.isArray(parsed))
       throw "JSON.parse should have returned an Array!";
     dashboardIssue = parsed.find(
-      (it) => it.title === defaultJson.dependencyDashboardTitle,
+      (it): it is GithubIssue =>
+        it.title === defaultJson.dependencyDashboardTitle,
     );
   }
   if (!dashboardIssue)
     throw `Not able to get dashboard issue for ${OWNER_REPO}`;
-  console.log("detected ", dashboardIssue.url);
-  if (dashboardIssue.body.includes("- [x]")) {
-    console.log(
-      "Dashboard already contains checked checkbox, not triggering manual update.",
-    );
-    return;
-  }
-  const manualTrigger = "- [ ] <!-- manual job -->";
+
+  console.log("Picked issue", dashboardIssue.url, "\n", dashboardIssue.title);
   if (!dashboardIssue.body.includes(manualTrigger)) {
     throw "Dashboard does not contain manual trigger checkbox, exiting.";
+  }
+  if (dashboardIssue.body.includes(checkedCheckbox)) {
+    return "Dashboard already contains checked checkbox, not triggering manual update.";
   }
   console.log(
     `Triggering renovate via ${OWNER_REPO}#${dashboardIssue.number}...`,
@@ -75,9 +67,9 @@ export const trigger = async ({ exec }: AsyncFunctionArguments) => {
   const bodyFile = path.join(import.meta.dirname, ".dashboard-body.md");
   writeFileSync(
     bodyFile,
-    dashboardIssue.body.replace(manualTrigger, "- [x] <!-- manual job -->"),
+    dashboardIssue.body.replace(manualTrigger, manuallyTriggered),
   );
-  await exec.getExecOutput("gh", [
+  await gh(
     "issue",
     "edit",
     dashboardIssue.number,
@@ -85,6 +77,32 @@ export const trigger = async ({ exec }: AsyncFunctionArguments) => {
     OWNER_REPO,
     "--body-file",
     bodyFile,
-  ]);
+  );
   return "Done.";
 };
+
+const checkedCheckbox = "- [x]";
+const manualTrigger = "- [ ] <!-- manual job -->";
+const manuallyTriggered = "- [x] <!-- manual job -->";
+
+const ghExec = ({
+  getExecOutput,
+}: Pick<AsyncFunctionArguments["exec"], "getExecOutput">) => {
+  async function gh(...args: (string | number)[]) {
+    return await getExecOutput(
+      "gh",
+      args.map((it) => `${it}`),
+    );
+  }
+  async function ghJson<T>(...args: (string | number)[]): Promise<T> {
+    return JSON.parse((await gh(...args)).stdout);
+  }
+
+  return { gh, ghJson };
+};
+type GithubIssue = Readonly<{
+  body: string;
+  number: number;
+  title: string;
+  url: string;
+}>;
