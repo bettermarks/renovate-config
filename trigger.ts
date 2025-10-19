f52e2f1d745fdf5c06ba3657b1fb0c7c281955d9
karfau <christian.bewernitz@bettermarks.com>
2025-10-19 07:42:28 +0200

    fix: prevent trigger when there is an open and mergeable PR

--- trigger.ts
+++ trigger.ts
@@ -11,79 +11,104 @@ import path from "node:path";
  * If another checkbox is checked in the issue, nothing will be triggered.
  * The function executed the `gh` CLI to talk to the GitHub API.
  */
-export const trigger = async ({ exec }: AsyncFunctionArguments) => {
+export const trigger = async ({
+  context,
+  core,
+  exec,
+}: AsyncFunctionArguments) => {
   const { gh, ghJson } = ghExec(exec);
   await gh("auth", "status");
+  const log = (message: string) => {
+    console.log(message);
+    core.summary.addRaw(message, true);
+    return message;
+  };
+  try {
+    // parse input
+    const OWNER_REPO = process.env.OWNER_REPO ?? "bettermarks/renovate-config";
+    const repoArgs = ["--repo", OWNER_REPO] as const;
+    const DASHBOARD_ISSUE = parseInt(process.env.DASHBOARD_ISSUE || "0");
+    console.log("Inputs:", { OWNER_REPO, DASHBOARD_ISSUE });
 
-  // parse input
-  const OWNER_REPO = process.env.OWNER_REPO ?? "bettermarks/renovate-config";
-  const DASHBOARD_ISSUE = parseInt(process.env.DASHBOARD_ISSUE || "0");
-  console.log("Inputs:", { OWNER_REPO, DASHBOARD_ISSUE });
+    let dashboardIssue: GithubIssue | undefined;
+    if (DASHBOARD_ISSUE) {
+      dashboardIssue = await ghJson<GithubIssue>(
+        "issue",
+        "view",
+        DASHBOARD_ISSUE,
+        ...repoArgs,
+        ...jsonIssue,
+      );
+    } else {
+      const parsed = await ghJson<ReadonlyArray<GithubIssue> | unknown>(
+        "issue",
+        "list",
+        ...authorRenovate,
+        ...repoArgs,
+        ...jsonIssue,
+      );
+      if (!Array.isArray(parsed))
+        throw "JSON.parse should have returned an Array!";
+      dashboardIssue = parsed.find(
+        (it): it is GithubIssue =>
+          it.title === defaultJson.dependencyDashboardTitle,
+      );
+    }
+    if (!dashboardIssue)
+      throw `Not able to get dashboard issue for ${OWNER_REPO}`;
 
-  let dashboardIssue: GithubIssue | undefined;
-  if (DASHBOARD_ISSUE) {
-    dashboardIssue = await ghJson<GithubIssue>(
-      "issue",
-      "view",
-      `${DASHBOARD_ISSUE}`,
-      "--repo",
-      OWNER_REPO,
-      "--json",
-      "url,number,body,title",
+    log(
+      `Picked issue ${dashboardIssue.url} with title "${dashboardIssue.title}".`,
     );
-  } else {
-    const parsed = await ghJson<ReadonlyArray<GithubIssue> | unknown>(
-      "issue",
+    if (!dashboardIssue.body.includes(manualTrigger)) {
+      throw "Issue does not contain manual trigger checkbox, exiting.";
+    }
+    if (dashboardIssue.body.includes(checkedCheckbox)) {
+      return log(
+        "Dashboard already contains checked checkbox. No action triggered.",
+      );
+    }
+    const openRenovatePRs = await ghJson<ReadonlyArray<GithubPr>>(
+      "pr",
       "list",
-      "--repo",
-      OWNER_REPO,
-      "--author",
-      "renovate[bot]",
-      "--state",
-      "open",
-      "--json",
-      "url,number,body,title",
+      ...repoArgs,
+      ...authorRenovate,
+      ...jsonPr,
     );
-    if (!Array.isArray(parsed))
-      throw "JSON.parse should have returned an Array!";
-    dashboardIssue = parsed.find(
-      (it): it is GithubIssue =>
-        it.title === defaultJson.dependencyDashboardTitle,
+    if (openRenovatePRs.filter((pr) => pr.mergeable).length) {
+      openRenovatePRs.forEach((pr) =>
+        log(
+          `- #${pr.number} is open and ${pr.mergeable ? "mergeable" : "not mergeable"} (${pr.commits.length} commits)`,
+        ),
+      );
+      return log("There are open and mergeable renovate PRs. No action taken.");
+    }
+    console.log(
+      `Triggering renovate via ${OWNER_REPO}#${dashboardIssue.number}...`,
     );
+    const bodyFile = path.join(import.meta.dirname, ".dashboard-body.md");
+    writeFileSync(
+      bodyFile,
+      dashboardIssue.body.replace(manualTrigger, manuallyTriggered),
+    );
+    await gh(
+      "issue",
+      "edit",
+      dashboardIssue.number,
+      "--body-file",
+      bodyFile,
+      ...repoArgs,
+    );
+    return "Done.";
+  } finally {
+    await core.summary.write();
   }
-  if (!dashboardIssue)
-    throw `Not able to get dashboard issue for ${OWNER_REPO}`;
-
-  console.log("Picked issue", dashboardIssue.url, "\n", dashboardIssue.title);
-  if (!dashboardIssue.body.includes(manualTrigger)) {
-    throw "Dashboard does not contain manual trigger checkbox, exiting.";
-  }
-  if (dashboardIssue.body.includes(checkedCheckbox)) {
-    return "Dashboard already contains checked checkbox, not triggering manual update.";
-  }
-  console.log(
-    `Triggering renovate via ${OWNER_REPO}#${dashboardIssue.number}...`,
-  );
-  const bodyFile = path.join(import.meta.dirname, ".dashboard-body.md");
-  writeFileSync(
-    bodyFile,
-    dashboardIssue.body.replace(manualTrigger, manuallyTriggered),
-  );
-  await gh(
-    "issue",
-    "edit",
-    dashboardIssue.number,
-    "--repo",
-    OWNER_REPO,
-    "--body-file",
-    bodyFile,
-  );
-  return "Done.";
 };
 
 const checkedCheckbox = "- [x]";
 const manualTrigger = "- [ ] <!-- manual job -->";
 const manuallyTriggered = "- [x] <!-- manual job -->";
+const authorRenovate = ["--author", "renovate[bot]"] as const;
 
 const ghExec = ({
   getExecOutput,
@@ -100,9 +125,26 @@ const ghExec = ({
 
   return { gh, ghJson };
 };
+
+const jsonIssue = ["--json", "url,number,body,title"];
 type GithubIssue = Readonly<{
   body: string;
   number: number;
   title: string;
+  url: string;
+}>;
+const jsonPr = [
+  "--json",
+  "autoMergeRequest,commits,mergeable,number,reviewDecision,statusCheckRollup,title,updatedAt,url",
+];
+type GithubPr = Readonly<{
+  autoMergeRequest: unknown;
+  commits: readonly unknown[];
+  mergeable: unknown;
+  number: number;
+  reviewDecision: string;
+  statusCheckRollup: unknown;
+  title: string;
+  updatedAt: string;
   url: string;
 }>;
